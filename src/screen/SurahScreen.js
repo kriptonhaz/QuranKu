41e8062bdc5806a6154c5bc43b680c3e1862bb5d
kriptonhaz <kriptonhaz@gmail.com>
2021-10-29 20:50:38 +0700

    fix surah list screen

--- src/screen/SurahScreen.js
+++ src/screen/SurahScreen.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { View, Text, StyleSheet, FlatList, Dimensions, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import SurahList from "../assets/data/surah-list.json";
+
+const { width, height } = Dimensions.get("screen");
 
 export function SurahScreen(){
   const navigation = useNavigation();
@@ -9,9 +12,40 @@ export function SurahScreen(){
     navigation.navigate("SurahDetail");
   }
 
+  const CardSurah = ({name, number, ayat, translation}) => {
+    return(
+      <TouchableOpacity style={styles.containerCardSurah} onPress={gotoDetail}>
+        <View style={styles.containerNumber}>
+          <Text style={styles.textNumber}>{number}</Text>
+        </View>
+        <View>
+          <Text style={styles.textSurah}>{`Surah ${name}`}</Text>
+          <Text style={styles.textAyat}>{`${translation} - ${ayat} Ayat`}</Text>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+
   return(
     <View style={styles.rootContainer}>
-      <Text style={styles.text} onPress={gotoDetail}>Surah Screen</Text>
+      <FlatList 
+        data={SurahList.data}
+        renderItem={({item}) => {
+          return(
+            <CardSurah 
+              number={item.id}
+              name={item.surat_name}
+              translation={item.surat_terjemahan}
+              ayat={item.count_ayat}
+            />
+          )
+        }}
+        ItemSeparatorComponent={() => {
+          return(
+            <View style={styles.separator}/>
+          )
+        }}
+      />
     </View>
   )
 }
@@ -21,7 +55,29 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
   },
-  text: {
-    color: 'black'
+  containerCardSurah: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  containerNumber: {
+    width: width*0.2,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textNumber: {
+    color: 'black',
+    fontSize: 30
+  },
+  textSurah: {
+    color: 'black',
+    fontSize: 28
+  },
+  textAyat: {
+    color: 'black',
+    fontSize: 20
+  },
+  separator: {
+    width: width,
+    height: 20
   }
 })
